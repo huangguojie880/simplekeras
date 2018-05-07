3d9930d1cfc4a3a9fa20136087fdba1bfb03714a
huangguojie880 <1583649520@qq.com>
2018-05-07 21:27:07 +0800

    Update applications.py

--- applications.py
+++ applications.py
@@ -10,3 +10,14 @@ def VGG19(img,out='block5_pool'):
     '''
     base_model = keras.applications.VGG19(input_tensor=img, include_top=False)
     return base_model.get_layer(out).output
+
+def VGG16(img,out='block5_pool'):
+    '''
+    Get the output of img via any layer of VGG16
+    The structure of VGG16:https://pan.baidu.com/s/1k36U8qRtt3VH5v4P4XSKLg
+    :param img: input layer
+    :param out: conv output name
+    :return: conv output
+    '''
+    base_model = keras.applications.VGG16(input_tensor=img, include_top=False)
+    return base_model.get_layer(out).output
