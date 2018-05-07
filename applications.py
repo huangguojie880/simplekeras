e45f73efa79def5581b1cd077fb1c2333d4e0df8
huangguojie880 <1583649520@qq.com>
2018-05-07 13:59:06 +0800

    Update applications.py

--- applications.py
+++ applications.py
@@ -1,9 +1,10 @@
-def VGG19(img,out='MaxPooling2D'):
+def VGG19(img,out='block5_pool'):
     '''
     Get the output of img via any layer of VGG19
+    The structure of VGG19:https://pan.baidu.com/s/18Za5Cn_slJZuvim2yZGvAA
     :param img: input layer
     :param out: conv output name
-    :return: conv output 
+    :return: conv output
     '''
     base_model = keras.applications.VGG19(input_tensor=img, include_top=False)
     return base_model.get_layer(out).output
