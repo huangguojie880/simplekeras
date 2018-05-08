3473aadf9b91ddbeb2bdbcd241f30abbb453c687
huangguojie880 <1583649520@qq.com>
2018-05-08 13:03:43 +0800

    Update losse.py

--- losse.py
+++ losse.py
@@ -10,3 +10,12 @@ def focal_mse(r = 1):
     def mse(y_true, y_pred):
         return K.mean((tf.abs(y_true - y_pred)**r)*tf.losses.sigmoid_cross_entropy( y_pred,y_true))
     return mse
+
+def Kl_loss(mu,l_sigma):
+    '''
+    Calculate the distance from the standard normal distribution
+    :param mu: Mean variables
+    :param l_sigma:Variance variable
+    :return:Kl loss
+    '''
+    return (0.5 * K.sum(K.exp(l_sigma) + K.square(mu) - 1. - l_sigma, axis=1))
